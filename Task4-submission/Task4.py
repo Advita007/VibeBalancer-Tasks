a24b42ae17c961d8179586e68a3e420c29bbba19
Advita007 <75312319+Advita007@users.noreply.github.com>
2026-05-04 14:28:09 +0530

    Update Task4.py

--- Task4-submission/Task4.py
+++ Task4-submission/Task4.py
@@ -27,7 +27,7 @@ def work(delay: Optional[int] = None):
 
 # In the week 3 server, the requests run one after the other, as the server is synchronous. The total time taken is atleast 6s, as the second request always has to wait until the first one is completed. The server is blocking.
 
-url = "http://127.0.0.1:8001/work?delay=3"
+url = "http://127.0.0.1:8002/work?delay=3"
 start = time.perf_counter()
 for _ in range(5):
     response = requests.get(url)
